d71b658a079d260cabee8d572f29bc3c6209c73f
Jasmeet Singh <jassingh1773@gmail.com>
2025-08-08 11:30:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,65 +3,86 @@ import pandas as pd
 import random
 from sklearn.preprocessing import StandardScaler
 import pickle
-import warnings
-import time
-warnings.filterwarnings("ignore")
 
-# Title
+col = ['MedInc',
+ 'HouseAge',
+ 'AveRooms',
+ 'AveBedrms',
+ 'Population',
+ 'AveOccup',
+ 'Longitude']
+st.title('California Housing Price Prediction')
 
-col=[ 'MedInc' , 'HouseAge' , 'AveRooms' , 'AveBedrms' , 'Population' , 'AveOccup' ]
+st.image('''https://jasonbarryteam.com/wp-content/uploads/2021/07/The-Bradbury-Estate-and-the-Rolls-Royce-Ghost-3-1024x613.jpg''')
 
-st.title("Calafornia Housing Price Prediction") #this give the title of our page
+st.header('Model of housing prices to predict median house values in California',divider = True)
 
-st.image("https://nycdsa-blog-files.s3.us-east-2.amazonaws.com/2021/03/chaitali-majumder/house-price-497112-KhCJQICS.jpg") #this is our image show in main page
+# st.subheader('''User must enter given values to predict prices:
+# ['MedInc',
+#  'HouseAge',
+#  'AveRooms',
+#  'AveBedrms',
+#  'Population',
+#  'AveOccup',
+#  'Longitude']
+# ''')
 
-st.header("model of housing prices to predict median house values in California",divider=True) #its our header of the site, and divider is used to divide our header 
+st.sidebar.title('Select House Features 🏡')
 
-#st.subheader('''User Must Enter Values To Predict Price:
-#[ 'MedInc' , 'HouseAge' , 'AveRooms' , 'AveBedrms' , 'Population' , 'AveOccup' ]''') #this is subheader
+st.sidebar.image('''https://imageio.forbes.com/blogs-images/amydobson/files/2019/01/630-Nimes-Road-Exterior-1200x722.jpg?format=jfor i in df[col]:pg&height=900&width=1600&fit=bounds''')
 
-st.sidebar.title("Select House Feature 🏠") #this give the side bar title
+#read data
+temp_df = pd.read_csv('California.csv')
 
-st.sidebar.image("https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?cs=srgb&dl=landscape-sky-clouds-259588.jpg&fm=jpg") #this give an image in our side bar
+random.seed(12)
 
-# This give the min and max value of the house in our california.csv file
-# and also read the data  
-temp_df=pd.read_csv("California.csv")
-all_Value=[]
-random.seed(57) #this will the random value so, that it cannot be change 
+all_values = []
 for i in temp_df[col]:
-    min_Value,max_value=temp_df[i].agg(['min','max'])
-    
-    var= st.sidebar.slider(f"Select {i} value", int(min_Value), int(max_value),random.randint(int(min_Value), int(max_value)))
+    min_value, max_value = temp_df[i].agg(['min','max'])
 
-    all_Value.append(var)
+    var = st.sidebar.slider(f'Select{i} range',int(min_value),int(max_value), 
+                      random.randint(int(min_value),int(max_value)))
 
-ss=StandardScaler()
+    all_values.append(var)
+
+ss = StandardScaler()
 ss.fit(temp_df[col])
-final_value=ss.transform([all_Value])
 
-with open("house_price_pred_ridge_model.pkl","rb")as f:
-    chatgpt=pickle.load(f)
+final_value = ss.fit_transform([all_values])
 
-price=chatgpt.predict(final_value)[0]*1000000
 
-st.write(pd.DataFrame(dict(zip(col,all_Value)),index=[1])) #zip is basically used for merge/add 2 data in a single data
-progress_bar=st.progress(0)
-placeholder=st.empty()
-placeholder.subheader("Predicting Price !!")
-place=st.empty()
-place.image("https://i.gifer.com//7Fmb.gif",width=100)
+
+with open('house_price_pred_ridge_model.pkl','rb')as f:
+    chatgpt = pickle.load(f)
+
+
+price = chatgpt.predict(final_value)[0]
+
+import time
+
+# st.write(pd.DataFrame(dict(zip(col,all_values)),indexx =  [1]))
+progress_bar = st.progress(0)
+placeholder = st.empty()
+placeholder.subheader('Predicting Price')
+
+place = st.empty()
+place.image('https://cdn.dribbble.com/userupload/22273130/file/original-b60664b066bca5c94894cf105d1190f8.gif',width = 80)
+
+
+
 
 if price>0:
+    
     for i in range(100):
-        time.sleep(0.05)
-        progress_bar.progress(i+1)
-    body=f"Predicted Median House Price ${round(price,2)}"
+        time.sleep(0.005)
+        progress_bar.progress(i + 1)
+
+        
+    body = f'Predicted Median House Values Price: ${round(price,2)} Thousand Dollars'
     placeholder.empty()
     place.empty()
     st.success(body)
-    
-else:
-    body="Incalid House feature"
 
+else:
+    body = ('Invalid House Features Values')
     st.warning(body)
